f5d040bd2f195a35ada7116a0e95ac13e4e6224c
Dmitrii <dmitrii@amberoad.ai>
2023-03-19 11:47:25 +0100

    feat: add openai to archive

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 import os
 import urllib3
 import random
-#import openai
+import openai
 
 BOT_ID = int(os.environ.get('BOT_ID'))
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
@@ -17,24 +17,22 @@ http = urllib3.PoolManager()
 
 class openaiClient:
     def __init__(self) -> None:
-        #openai.api_key = OPENAI_KEY
-        pass
+        openai.api_key = OPENAI_KEY
     
     def complete_chat(self, user_message: str):
-        # response = openai.ChatCompletion.create(
-        #     model=OPENAI_MODEL,
-        #     messages = [
-        #         {"role": "system", "content": "Ты чат-бот с открытым исходным кодом, который использует OpenAI GPT-3.5 для генерации ответов на вопросы. Пиши краткие сообщения."},
-        #         {"role": "user", "content": user_message},
-        #     ],
-        #     temperature=1.0,
-        #     max_tokens=128,
-        #     frequency_penalty=1.0,
-        #     presence_penalty=1.0,
-        # )
-        # answer = response["choices"][0]["message"]["content"]
-        # print(answer)
-        answer=user_message
+        response = openai.ChatCompletion.create(
+            model=OPENAI_MODEL,
+            messages = [
+                {"role": "system", "content": "Ты чат-бот с открытым исходным кодом, который использует OpenAI GPT-3.5 для генерации ответов на вопросы. Пиши краткие сообщения."},
+                {"role": "user", "content": user_message},
+            ],
+            temperature=1.0,
+            max_tokens=128,
+            frequency_penalty=1.0,
+            presence_penalty=1.0,
+        )
+        answer = response["choices"][0]["message"]["content"]
+        print(answer)
         return answer
 
 
