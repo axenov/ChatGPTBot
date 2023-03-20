d5313274886ed86c5b3972af983758d40bf6b5b7
Dmitrii <dmitrii@amberoad.ai>
2023-03-20 19:25:56 +0100

    fix: should_reply always in pm

--- telegram_client.py
+++ telegram_client.py
@@ -45,10 +45,8 @@ class telegramClient:
     
     def should_reply(self, message:dict):
         """ The function that decides whether the bot should reply to a message or not """
-        print(message["from"]["id"])
-        print(message["chat"]["id"])
         if (
-            (message["from"]["id"] == message["chat"]["id"]) and
+            (message["from"]["id"] == message["chat"]["id"]) or
             ("reply_to_message" in message and message["reply_to_message"]["from"]["id"] == BOT_ID) or
             ("entities" in message and message["entities"][0]["type"] == "mention" and ("@" + BOT_NAME) in message["text"])
         ):
